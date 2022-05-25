aa5ecdbd9fc63a95ff8b163fdfd186fd90d58890
oyvindhaugen <oyvindet@gmail.com>
2022-05-25 15:56:00 +0200

    Added random generator it can pick from array
    need to:
    - Make it remove from array when name is picked
    - add good UI

--- src/main/java/com/oyvindh/app.java
+++ src/main/java/com/oyvindh/app.java
@@ -1,4 +1,41 @@
 package com.oyvindh;
 
-public class app {
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class app extends JFrame {
+    static JFrame frame;
+    static JButton generate;
+    static JLabel name;
+    static void getRandomNumber(){
+        String[] arr = {"John", "Bobby", "Julian"};
+        Random r = new Random();
+        int randomNumber = r.nextInt(arr.length);
+        name.setText(arr[randomNumber]);}
+    public static void main(String[] args) {
+        frame = new JFrame("Random Name Generator");
+        generate = new JButton("Click");
+        name = new JLabel();
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3,3));
+        panel.add(generate);
+        panel.add(name);
+        generate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getRandomNumber();
+            }
+        });
+        frame.add(panel);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setSize(500, 500);
+
+
+    }
 }
