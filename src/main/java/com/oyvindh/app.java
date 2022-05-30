72532ce40c9796b2a510cc4434c0b8262e97e76e
oyvindhaugen <oyvindet@gmail.com>
2022-05-30 16:44:37 +0200

    Changed random from Array to List
    - make it remove names

--- src/main/java/com/oyvindh/app.java
+++ src/main/java/com/oyvindh/app.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class app extends JFrame {
@@ -12,18 +14,26 @@ public class app extends JFrame {
     static JButton generate;
     static JLabel name;
     static void getRandomNumber(){
-        String[] arr = {"John", "Bobby", "Julian"};
+        List<String> list = new ArrayList<>();
+        list.add(0, "John");
+        list.add(1, "Eden");
+        list.add(2, "Peter");
+        list.add(3, "Ethan");
         Random r = new Random();
-        int randomNumber = r.nextInt(arr.length);
-        name.setText(arr[randomNumber]);}
+
+        int randomItem = r.nextInt(list.size());
+        String randomElement = list.get(randomItem);
+        name.setText(randomElement);
+        }
     public static void main(String[] args) {
         frame = new JFrame("Random Name Generator");
         generate = new JButton("Click");
         name = new JLabel();
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(3,3));
+        panel.setLayout(new GridLayout(2,1));
         panel.add(generate);
         panel.add(name);
+        name.setHorizontalAlignment(SwingConstants.CENTER);
         generate.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
